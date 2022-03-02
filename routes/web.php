11cf5d81b293f16b874db9708ea84b7fc3e16eee
Elias Zech <elias.zech@outllook.com>
2022-03-02 15:45:37 +0100

    Added Active Directory Support

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function() {
-    Route::get('/', [\App\Http\Controllers\Controller::class, 'index']);
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', [\App\Http\Controllers\Controller::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
+
