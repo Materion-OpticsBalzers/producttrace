f53eb981313420d7935eda200f224358b5cbd7f3
eliaszech <elias.zech@outlook.com>
2022-03-11 21:29:31 +0100

    Added rejection module

---
+++ app/Http/Livewire/Blocks/Arc.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Livewire\Blocks;
+
+use App\Models\Data\Process;
+use App\Models\Data\Wafer;
+use App\Models\Generic\Block;
+use App\Models\Generic\Rejection;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class Arc extends Component
+{
+    public $blockId;
+    public $orderId;
+    public $prevBlock;
+    public $nextBlock;
+
+    public $search = '';
+    public $machine = null;
+    public $calculatedPosition = 'Aussen';
+
+    public $selectedWafer = null;
+
+    public function checkWafer($waferId) {
+        if($waferId == '') {
+            $this->addError('wafer', 'Die Wafernummer darf nicht leer sein!');
+            return false;
+        }
+
+        $wafer = Wafer::find($waferId);
+
+        if($wafer == null) {
+            $this->addError('wafer', 'Dieser Wafer ist nicht vorhanden!');
+            return false;
+        }
+
+        if($wafer->rejected){
+            if($this->nextBlock != null) {
+                $nextWafer = Process::where('wafer_id', $wafer->id)->where('order_id', $this->orderId)->where('block_id', $this->nextBlock)->first();
+                if($nextWafer == null) {
+                    $this->addError('wafer', "Dieser Wafer wurde in " . $wafer->rejection_order . " -> " . $wafer->rejection_avo . " " . $wafer->rejection_position . " als Ausschuss markiert.");
+                    return false;
+                }
+            } else {
+                $this->addError('wafer', "Dieser Wafer wurde in " . $wafer->rejection_order . " -> " . $wafer->rejection_avo . " " . $wafer->rejection_position . " als Ausschuss markiert.");
+                return false;
+            }
+        }
+
+        if($wafer->reworks == 2) {
+            $this->addError('wafer', 'Dieser Wafer darf nicht mehr verwendet werden.');
+            return false;
+        }
+
+        if($this->prevBlock != null) {
+            $prevWafer = Process::where('wafer_id', $wafer->id)->where('order_id', $this->orderId)->where('block_id', $this->prevBlock)->first();
+            if ($prevWafer == null) {
+                $this->addError('wafer', 'Dieser Wafer existiert nicht im vorherigen Schritt!');
+                return false;
+            }
+        }
+
+        if(Process::where('wafer_id', $wafer->id)->where('order_id', $this->orderId)->where('block_id', $this->blockId)->exists()) {
+            $this->addError('wafer', 'Dieser Wafer wurde schon verwendet!');
+            return false;
+        }
+
+        return true;
+    }
+
+    public function addEntry($order, $block, $operator, $box) {
+        $error = false;
+
+        if(!$this->checkWafer($this->selectedWafer)) {
+            $this->addError('response', 'Ein Fehler mit der Wafernummer hat das Speichern verhindert');
+            $error = true;
+        }
+
+        if($operator == '') {
+            $this->addError('operator', 'Der Operator darf nicht leer sein!');
+            $error = true;
+        }
+
+        if($box == '') {
+            $this->addError('box', 'Die Box ID Darf nicht leer sein!');
+            $error = true;
+        }
+
+        if($error)
+            return false;
+
+        Process::create([
+            'wafer_id' => $this->selectedWafer,
+            'order_id' => $order,
+            'block_id' => $block,
+            'operator' => $operator,
+            'box' => $box,
+            'date' => now()
+        ]);
+
+        session()->flash('success', 'Eintrag wurde erfolgreich gespeichert!');
+    }
+
+    public function removeEntry($entryId) {
+        $process = Process::find($entryId);
+
+        $process->delete();
+    }
+
+    public function clear($order, $block) {
+        $wafers = Process::where('order_id', $order)->where('block_id', $block)->with('wafer');
+
+        $wafers->delete();
+    }
+
+    public function render()
+    {
+        $block = Block::find($this->blockId);
+
+        $wafers = Process::where('order_id', $this->orderId)->where('block_id', $this->blockId)->with('rejection')->orderBy('wafer_id', 'asc')->lazy();
+
+        if($this->search != '') {
+            $wafers = $wafers->filter(function ($value, $key) {
+                return stristr($value->wafer_id, $this->search);
+            });
+        }
+
+        if($this->selectedWafer != '')
+            $sWafers = Wafer::where('id', 'like', "%$this->selectedWafer%")->limit(30)->get();
+        else
+            $sWafers = [];
+
+        if($wafers->count() >= 9 && $wafers->count() < 13)
+            $this->calculatedPosition = 'Mitte';
+        elseif($wafers->count() >= 13)
+            $this->calculatedPosition = 'Zentrum';
+
+        if($this->machine == null)
+            $this->machine = DB::connection('oracle')->select("SELECT FSNR FROM PROD_ERP_001.PRDOP WHERE PRDOP.PRDNR = '$this->orderId' AND POSNR = 1")[0]->fsnr;
+
+        return view('livewire.blocks.arc', compact('block', 'wafers', 'sWafers'));
+    }
+}
