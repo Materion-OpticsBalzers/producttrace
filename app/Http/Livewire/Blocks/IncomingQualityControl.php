eae2b8c3d1a7b93f7b03e8147ba3f6484b1dd4e3
Elias Zech <elias.zech@outllook.com>
2022-05-16 08:19:47 +0200

    Added the ability to track Wafers

--- app/Http/Livewire/Blocks/IncomingQualityControl.php
+++ app/Http/Livewire/Blocks/IncomingQualityControl.php
@@ -31,9 +31,9 @@ class IncomingQualityControl extends Component
 
     public function getScannedWafer() {
         $scan = Scan::where('block_id', $this->blockId)->first();
-        $wafer = Wafer::find($scan->value);
 
         if ($scan != null) {
+            $wafer = Wafer::find($scan->value);
             $this->updateWafer($scan->value, $wafer->box ?? '');
             $scan->delete();
         }
